4e3e699dbc2454061960b857ad3e94020f216ea2
unknown <wilusz1@o2.pl>
2020-03-16 15:10:50 +0100

    add build to repozytory for serwer

---
+++ build/precache-manifest.7ab0d962c239c9729b799188dc58374d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3252633bc9d32625696c2d27615dce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aece6783c29c28b9efc",
+    "url": "/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "884f89a5f9d4b152cee8",
+    "url": "/static/css/main.668e6c10.chunk.css"
+  },
+  {
+    "revision": "9aece6783c29c28b9efc",
+    "url": "/static/js/2.1722144c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.1722144c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884f89a5f9d4b152cee8",
+    "url": "/static/js/main.6141371a.chunk.js"
+  },
+  {
+    "revision": "726a8ee78e7162a104f7",
+    "url": "/static/js/runtime-main.456b63fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
